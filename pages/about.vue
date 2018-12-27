2f210668846491e9bcb8d3b8268845448fee74e0
takashi tokumoto <jazzm@ster.jp.net>
2018-12-28 01:28:38 +0900

    initial

---
+++ pages/about.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1 class="title">About this App</h1>
+
+    <p>アプリケーションの紹介文がここに入ります。</p>
+
+    <router-link to="/">トップページへ戻る</router-link>    
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1 class="title">My Blog App</h1>
+    <p></p>
+    <ul>
+        <li><router-link to="/about">トップページへ戻る</router-link> </li>
+    </ul>
+  </div>
+</template>
