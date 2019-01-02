9465f61d2976896bec2701747a7816d6bd2a58fa
takashi tokumoto <jazzm@ster.jp.net>
2019-01-03 08:50:01 +0900

    配列作ってfor

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,35 @@
 <template>
   <div>
     <h1 class="title">My Blog App</h1>
-    <p></p>
     <ul>
-        <li><router-link to="/about">トップページへ戻る</router-link> </li>
+        <li v-for="post in posts" :key="post.id">
+          {{post.title}}
+        </li>
     </ul>
+    <router-link to="/about">トップページへ戻る</router-link>
   </div>
+  
 </template>
+
+<script>
+export default {
+  data(){
+    return {
+      posts: [
+        {
+          "id": 1,
+          "title": "最初の記事です。"
+        },
+        {
+          "id": 2,
+          "title": "2番目の投稿です。",
+        },
+        {
+          "id": 3,
+          "title": "３番めの投稿です。",
+        },
+      ]      
+    }
+  }  
+}
+</script>
