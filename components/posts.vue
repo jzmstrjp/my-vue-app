03dc8103e091c8a3a5c027bbef89ef666904cd9b
takashi tokumoto <jazzm@ster.jp.net>
2019-01-03 13:54:53 +0900

    関連リンク

--- components/posts.vue
+++ components/posts.vue
@@ -4,8 +4,8 @@ export default [
       "title": "タグに色つけ太郎",
       "body": "テキストエディタの拡張機能です。\nhtmlのタグ毎に色づけし、目的のタグを見つけやすくしてくれます。\nAtom・Brackets・VSCode版を作成・公開し、\nBrackets版は約8万ダウンロードを記録しました。",
       "img": "color-the-tag-name.png",
-      "url": "https://marketplace.visualstudio.com/items?itemName=jzmstrjp.color-the-tag-name",
-      "link_text": "Webサイト",
+      "url": "https://github.com/jzmstrjp/atom-color-the-tag-name",
+      "link_text": "GitHub",
     },
     {
       "title": "ジャズマスター.jp",
