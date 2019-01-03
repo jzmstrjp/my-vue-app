ad155872954e1e1158e492ebb6dbd771a46d6bb0
takashi tokumoto <jazzm@ster.jp.net>
2019-01-03 10:40:29 +0900

    定期

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,7 @@
     <h1 class="title">My Blog App</h1>
     <ul class="posts_list">
       <li v-for="(post,key) in posts" :key="key">
-        <router-link :to="{ name: 'posts-id', params: { id: key }}">User</router-link>
-        <router-link to="/posts/">
+        <router-link :to="{ name: 'posts-id', params: { id: key }}">
           <figure class="single_post_fig">
             <figcaption class="single_post_fig__caption">
               <p class="single_post__title">{{post.title}}</p>
