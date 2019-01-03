9ac390be90662a3fca7a167690d73c5bd4a1d7cc
takashi tokumoto <jazzm@ster.jp.net>
2019-01-03 10:21:42 +0900

    定期

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,15 @@
 <template>
   <div>
     <h1 class="title">My Blog App</h1>
-    <ul class="posts">
+    <ul class="posts_list">
       <li v-for="(post,key) in posts" :key="key">
         <router-link v-bind:to="{ name : 'Event', params : { id: key }}">あああ</router-link>
         <router-link to="/posts/">
-          <figure class="single_post">
-            <figcaption class="single_post__fig_caption">
+          <figure class="single_post_fig">
+            <figcaption class="single_post_fig__caption">
               <p class="single_post__title">{{post.title}}</p>
               <p class="single_post__body">{{post.body}}</p>
             </figcaption>
-            <div v-if="post.img" class="single_post__img">
-              <img v-bind:src="require('~/assets/images/' + post.img)">
-            </div>
           </figure>
         </router-link>
       </li>
--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -1,17 +1,28 @@
 <template>
-  <div>
-    <h1 class="title">About this App</h1>
-
-    <figure class="single_post">
-      <figcaption class="single_post__fig_caption">
-        <p class="single_post__title">{{post.title}}</p>
-        <p class="single_post__body">{{post.body}}</p>
+  <div class="single_post_page">
+    <h1 class="title">{{post.title}}</h1>
+    <h2>{{ $route.params.id }}</h2>
+    <figure class="single_post_fig">
+      <figcaption class="single_post_fig__caption">
+          <p class="single_post__body">{{post.body}}</p>
       </figcaption>
-      <div v-if="post.img" class="single_post__img">
-        <img v-bind:src="require('~/assets/images/' + post.img)">
+      <div v-if="post.img" class="single_post_fig__img">
+          <img v-bind:src="require('~/assets/images/' + post.img)">
       </div>
     </figure>
+   
 
     <router-link to="/">トップページへ戻る</router-link>    
   </div>
 </template>
+
+<script>
+import Posts from '~/components/posts.vue'
+export default {
+  data(){
+    return {
+      post: Posts[this.$route.params.id],
+    }
+  }  
+}
+</script>
