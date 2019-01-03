f4baf9aa53b40d88c93bb3f7b5d9655236fd1a58
takashi tokumoto <jazzm@ster.jp.net>
2019-01-03 09:37:50 +0900

    経過

--- components/posts.vue
+++ components/posts.vue
@@ -1,12 +1,10 @@
 <script>
-export default {
-  posts: [
+export default [
     {
       "title": "インポート記事",
       "body": "最初の記事です。",
       "genre": "プラグイン",
       "img": "color-the-tag-name.png",
     },
-  ],
-}
+  ]
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,17 @@
   <div>
     <h1 class="title">My Blog App</h1>
     <ul class="posts">
-        <li v-for="(post,key) in posts" :key="key">
-          <figure class="single_post">
-            <figcaption class="single_post__fig_caption">
-              <p class="single_post__title">{{post.title}}</p>
-              <p class="single_post__body">{{post.body}}</p>
-            </figcaption>
-            <div class="single_post__img">
-              <img src="~/assets/images/color-the-tag-name.png">
-            </div>
-          </figure>
-        </li>
+      <li v-for="(post,key) in posts" :key="key">
+        <figure class="single_post">
+          <figcaption class="single_post__fig_caption">
+            <p class="single_post__title">{{post.title}}</p>
+            <p class="single_post__body">{{post.body}}</p>
+          </figcaption>
+          <div class="single_post__img">
+            <img src="~/assets/images/{{post.img}}">
+          </div>
+        </figure>
+      </li>
     </ul>
     <router-link to="/about">トップページへ戻る</router-link>
   </div>
@@ -24,14 +24,7 @@ import Posts from '~/components/posts.vue'
 export default {
   data(){
     return {
-      posts: [
-        {
-          "title": "タグに色つけ太郎",
-          "body": "最初の記事です。",
-          "genre": "プラグイン",
-          "img": "color-the-tag-name.png",
-        },
-      ],
+      posts: Posts,
     }
   }  
 }
